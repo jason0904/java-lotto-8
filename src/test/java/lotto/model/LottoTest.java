0b296b741f624294100a2e77a2928438cd4ec470
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-28 16:08:26 +0000

    refactor(LottoTest):새로 수정한 로또 클래스에 맞게 테스트 코드 수정.

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -6,20 +6,23 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
     @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("로또 번호 개수 검증 테스트")
+    void LottoSizeValidationTest() {
+        assertThatThrownBy(() -> {
+            new Lotto(List.of(1, 2, 3, 4, 5));
+        }).isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("[ERROR] 로또 번호는 6개여야 합니다.");
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
-    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("로또 번호가 오름차순으로 정렬되서 나오는지 확인한다.")
+    void LottoNumberSortingTest() {
+        Lotto lotto = new Lotto(List.of(5, 3, 1, 4, 6, 2));
+        List<Integer> expected = List.of(1, 2, 3, 4, 5, 6);
+        assertEquals(expected, lotto.getNumbers());
     }
-
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 }
