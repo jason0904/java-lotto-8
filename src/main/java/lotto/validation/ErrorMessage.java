fa676bf85d8a3ab930afa8235f6897989d6370e1
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-28 15:43:30 +0000

    refactor(ErrorMessage): 로또 번호의 유횻값 범위에 따라 메시지를 동적으로 세팅할 수 있게끔 구조 변경.

--- src/main/java/lotto/validation/ErrorMessage.java
+++ src/main/java/lotto/validation/ErrorMessage.java
@@ -1,24 +1,25 @@
 package lotto.validation;
 
+import java.util.function.Supplier;
+
 public enum ErrorMessage {
-    ERROR_PURCHASE_AMOUNT_UNIT("구입 금액은 1,000원 단위로 입력해야 합니다."),
-    ERROR_PURCHASE_AMOUNT_POSITIVE("구입 금액은 양수여야 합니다."),
-    ERROR_PURCHASE_AMOUNT_ZERO("구입 금액은 0원일 수 없습니다."),
-    ERROR_PURCHASE_AMOUNT_MAX("최대 구입 금액을 초과했습니다."),
-    ERROR_LOTTO_NUMBER("로또 번호는 1부터 45 사이의 정수여야 합니다."),
-    ERROR_LOTTO_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
-    ERROR_DUPLICATE_LOTTO_NUMBER("로또 번호는 중복될 수 없습니다."),
-    ERROR_BONUS_NUMBER("보너스 번호는 로또 번호와 중복될 수 없습니다.");
+    ERROR_PURCHASE_AMOUNT_UNIT(() -> "구입 금액은 1,000원 단위로 입력해야 합니다."),
+    ERROR_PURCHASE_AMOUNT_POSITIVE(() -> "구입 금액은 양수여야 합니다."),
+    ERROR_PURCHASE_AMOUNT_ZERO(() -> "구입 금액은 0원일 수 없습니다."),
+    ERROR_PURCHASE_AMOUNT_MAX(() -> "최대 구입 금액을 초과했습니다."),
+    ERROR_LOTTO_NUMBER(() -> String.format("로또 번호는 %d부터 %d 사이의 정수여야 합니다.", LottoNumberValidation.MIN_NUMBER, LottoNumberValidation.MAX_NUMBER)),
+    ERROR_LOTTO_NUMBER_COUNT(() -> "로또 번호는 6개여야 합니다."),
+    ERROR_DUPLICATE_LOTTO_NUMBER(() -> "로또 번호는 중복될 수 없습니다."),
+    ERROR_BONUS_NUMBER(() -> "보너스 번호는 로또 번호와 중복될 수 없습니다.");
 
-    private final String ERROR_MARK = "[ERROR]";
-    private final String message;
+    private static final String ERROR_MARK = "[ERROR]";
+    private final Supplier<String> messageSupplier;
 
-    ErrorMessage(String message) {
-        this.message = ERROR_MARK + " " + message;
+    ErrorMessage(Supplier<String> messageSupplier) {
+        this.messageSupplier = messageSupplier;
     }
 
     public String getMessage() {
-        return message;
+        return ERROR_MARK + " " + messageSupplier.get();
     }
-
 }
