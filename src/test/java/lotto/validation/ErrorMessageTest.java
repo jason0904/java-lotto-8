511676d55e794ab05ac2a96e0eb9c04606b7bb8b
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-28 14:45:27 +0000

    test(ErrorMessage):에러메시지 형식 출력 테스트 추가.

---
+++ src/test/java/lotto/validation/ErrorMessageTest.java
@@ -0,0 +1,18 @@
+package lotto.validation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ErrorMessageTest {
+
+    @Test
+    @DisplayName("에러 메시지 형식 출력 테스트")
+    void testErrorMessageFormat() {
+        ErrorMessage errorMessage = ErrorMessage.ERROR_PURCHASE_AMOUNT_UNIT;
+        String expectedMessage = "[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.";
+        assertEquals(expectedMessage, errorMessage.getMessage());
+    }
+    
+}
