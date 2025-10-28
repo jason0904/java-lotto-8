7445e2f4e17e573489da341fa8fb672ac9f8714a
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-28 14:53:38 +0000

    feat(PurchaseAmount): 구매한 금액을 관리하는 PurchaseAmount 클래스 추가. 또한 금액 예외 처리도 추가.

---
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -0,0 +1,40 @@
+package lotto.model;
+
+import lotto.validation.ErrorMessage;
+
+public class PurchaseAmount {
+    private final Long purchaseAmount;
+
+    public PurchaseAmount(String purchaseAmountText) {
+        validate(purchaseAmountText);
+        purchaseAmount = Long.parseLong(purchaseAmountText);
+    }
+
+    private void validate(String purchaseAmountText) {
+        Long validatePurchaseAmount = 0L;
+        
+        try {
+            validatePurchaseAmount = Long.parseLong(purchaseAmountText);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_PURCHASE_AMOUNT_MAX.getMessage());
+        }
+
+        if(validatePurchaseAmount < 0) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_PURCHASE_AMOUNT_POSITIVE.getMessage());
+        }
+
+        if(validatePurchaseAmount == 0) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_PURCHASE_AMOUNT_ZERO.getMessage());
+        }
+
+        if(validatePurchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_PURCHASE_AMOUNT_UNIT.getMessage());
+        }
+
+    }
+
+    public Long getPurchaseAmount() {
+        return purchaseAmount;
+    }
+
+}
