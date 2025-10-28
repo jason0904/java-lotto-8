d9a8470b5c3d1fd0bd9570f2e7b787206befaa0f
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-28 14:28:44 +0000

    feat(ErrorMessage):에러 메시지 저장을 위한 enum 클래스 생성.

---
+++ src/main/java/lotto/validation/ErrorMessage.java
@@ -0,0 +1,26 @@
+package lotto.validation;
+
+public enum ErrorMessage {
+    ERROR_MARK("[ERROR]"),
+    ERROR_PURCHASE_AMOUNT("구입 금액은 1,000원 단위로 입력해야 합니다."),
+    ERROR_LOTTO_NUMBER("로또 번호는 1부터 45 사이의 정수여야 합니다."),
+    ERROR_LOTTO_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
+    ERROR_DUPLICATE_LOTTO_NUMBER("로또 번호는 중복될 수 없습니다."),
+    ERROR_BONUS_NUMBER("보너스 번호는 로또 번호와 중복될 수 없습니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return ErrorMessage.ERROR_MARK + " " + message;
+    }
+
+}
