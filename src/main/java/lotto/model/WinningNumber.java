32f7751b940420ee830a2a16e7605d51df2cd45e
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-29 09:45:30 +0000

    feat(WinningNumber):당첨번호를 저장하는 WinningNumber 클래스 추가. 예외 처리 추가.

---
+++ src/main/java/lotto/model/WinningNumber.java
@@ -0,0 +1,34 @@
+package lotto.model;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lotto.validation.LottoNumberValidation;
+import lotto.validation.LottoValidation;
+
+public class WinningNumber {
+
+    private final List<Integer> winningNumbers;
+
+    public WinningNumber(List<String> input) {
+        validate(input);
+        this.winningNumbers = input.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+    
+    private void validate(List<String> input) {
+        LottoValidation.validateLottoSize(input.size());
+
+        for(String number : input) {
+            LottoNumberValidation.validateNumberIsInteger(number);
+            LottoNumberValidation.validateNumberRange(Integer.parseInt(number));
+        }
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return Collections.unmodifiableList(winningNumbers);
+    }
+    
+}
