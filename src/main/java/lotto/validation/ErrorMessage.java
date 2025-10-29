1d1669f1cf7183977c0607d3e63d405213c8d3bd
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-29 10:30:41 +0000

    refactor(ErrorMessage):에러 메시지들 수정

--- src/main/java/lotto/validation/ErrorMessage.java
+++ src/main/java/lotto/validation/ErrorMessage.java
@@ -8,7 +8,8 @@ public enum ErrorMessage {
     PURCHASE_AMOUNT_ZERO(() -> "구입 금액은 0원일 수 없습니다."),
     PURCHASE_AMOUNT_MAX(() -> "최대 구입 금액을 초과했습니다."),
     WINNING_NUMBER_RANGE(() -> String.format("당첨 번호는 %d부터 %d 사이의 정수여야 합니다.", LottoRule.MIN_NUMBER.getValue(), LottoRule.MAX_NUMBER.getValue())),
-    WINNING_NUMBER_COUNT(() -> "당첨 번호는 6개여야 합니다."),
+    WINNING_NUMBER_SIZE(() -> "당첨 번호는 6개여야 합니다."),
+    LOTTO_SIZE(() -> "로또 번호는 6개여야 합니다."),
     DUPLICATE_WINNING_NUMBER(() -> "당첨 번호는 중복될 수 없습니다."),
     WINNING_NUMBER_FORMAT(() -> "당첨 번호는 쉼표(,)로 구분된 숫자여야 합니다."),
     DUPLICATE_BONUS_NUMBER(() -> "보너스 번호는 당첨 번호와 중복될 수 없습니다.");
