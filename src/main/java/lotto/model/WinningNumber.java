0f0718f10dcd2987e797fdffc072d0aafc35fe73
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-29 10:30:04 +0000

    feat(WinningNumber):중복숫자 예외처리 추가.

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -24,15 +24,17 @@ public class WinningNumber {
             validateNumberIsInteger(number);
             validateNumberRange(Integer.parseInt(number));
         }
+
+        validateDuplicateNumber(input);
     }
 
-    public static void validateLottoSize(int size) {
+    private void validateLottoSize(int size) {
         if (size != LottoRule.LOTTO_SIZE.getValue()) {
-            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_COUNT.getMessage());
+            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_SIZE.getMessage());
         }
     }
 
-    public static void validateNumberIsInteger(String numberText) {
+    private void validateNumberIsInteger(String numberText) {
         try {
             Integer.parseInt(numberText);
         } catch (NumberFormatException e) {
@@ -40,14 +42,25 @@ public class WinningNumber {
         }
     }
 
-    public static void validateNumberRange(int number) {
+    private void validateNumberRange(int number) {
         if (number < LottoRule.MIN_NUMBER.getValue() || number > LottoRule.MAX_NUMBER.getValue()) {
             throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_RANGE.getMessage());
+        }
+    }
+
+    private void validateDuplicateNumber(List<String> input) {
+        long distinctCount = input.stream()
+                .distinct()
+                .count();
+
+        if (distinctCount != input.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_WINNING_NUMBER.getMessage());
         }
     }
 
     public List<Integer> getWinningNumbers() {
         return Collections.unmodifiableList(winningNumbers);
     }
+
 
 }
