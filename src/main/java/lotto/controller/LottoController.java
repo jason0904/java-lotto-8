f9a13669c39e03f02573a12526690bb53ac96a3f
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-29 14:59:00 +0000

    feat(LottoController):로또 생성 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,23 +3,41 @@ package lotto.controller;
 import java.util.function.Supplier;
 
 import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
 import lotto.model.PurchaseAmount;
 import lotto.model.WinningNumber;
 import lotto.model.BonusNumber;
+import lotto.model.LottoMakeService;
+import lotto.model.LottoRepository;
 
 public class LottoController {
 
     private final LottoInputView lottoInputView;
+    private final LottoOutputView lottoOutputView;
+    private final LottoMakeService lottoMakeService;
 
-    public LottoController(LottoInputView lottoInputView) {
+    public LottoController(LottoInputView lottoInputView, LottoOutputView lottoOutputView, LottoMakeService lottoMakeService) {
         this.lottoInputView = lottoInputView;
+        this.lottoOutputView = lottoOutputView;
+        this.lottoMakeService = lottoMakeService;
     }
 
     public void run() {
         PurchaseAmount purchaseAmount = errorCatch(() -> lottoInputView.purchaseInput());
-        WinningNumber winningNumber = errorCatch(() -> lottoInputView.winningNumberInput());
-        BonusNumber bonusNumber = errorCatch(() -> lottoInputView.bonusNumberInput(winningNumber));
+        LottoRepository lottoRepository = makeLottos(purchaseAmount);
 
+        WinningNumber winningNumber = errorCatch(() -> lottoInputView.winningNumberInput());
+
+
+        BonusNumber bonusNumber = errorCatch(() -> lottoInputView.bonusNumberInput(winningNumber));
+        
+    }
+
+    private LottoRepository makeLottos(PurchaseAmount purchaseAmount) {
+        LottoRepository lottoRepository = lottoMakeService.makeLottos(purchaseAmount.getPurchaseAmount());
+        lottoOutputView.printLottos(lottoRepository, purchaseAmount);
+
+        return lottoRepository;
     }
 
     private <T> T errorCatch(Supplier<T> function) {
