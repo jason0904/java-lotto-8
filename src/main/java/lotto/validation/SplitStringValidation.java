090650954f36869d20956621845a546f07b16a91
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-29 11:11:15 +0000

    refactor(SplitStringValidation): 메서드를 편하게 꺼내쓰기 위해 둘의 메서드를 병합한 메서드를 사용할 수 있게함

--- src/main/java/lotto/validation/SplitStringValidation.java
+++ src/main/java/lotto/validation/SplitStringValidation.java
@@ -2,13 +2,18 @@ package lotto.validation;
 
 public class SplitStringValidation {
 
-    public static void validateStringStartWithComma(String input) {
+    public static void validateComma(String input) {
+        validateStringStartWithComma(input);
+        validateStringEndWithComma(input);
+    }
+
+    private static void validateStringStartWithComma(String input) {
         if (input.startsWith(",")) {
             throw new IllegalArgumentException(ErrorMessage.STRING_STARTS_WITH_COMMA.getMessage());
         }
     }
 
-    public static void validateStringEndWithComma(String input) {
+    private static void validateStringEndWithComma(String input) {
         if (input.endsWith(",")) {
             throw new IllegalArgumentException(ErrorMessage.STRING_ENDS_WITH_COMMA.getMessage());
         }
