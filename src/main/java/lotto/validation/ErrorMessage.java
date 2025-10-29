756beb751ea0587b5bf1dd76d613d6d6139cee06
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-29 09:53:58 +0000

    refactor(ErrorMessage):에러메시지 열겨형 상수명 수정, 에러 메시지 내용 수정.

--- src/main/java/lotto/validation/ErrorMessage.java
+++ src/main/java/lotto/validation/ErrorMessage.java
@@ -3,15 +3,15 @@ package lotto.validation;
 import java.util.function.Supplier;
 
 public enum ErrorMessage {
-    ERROR_PURCHASE_AMOUNT_UNIT(() -> "구입 금액은 1,000원 단위로 입력해야 합니다."),
-    ERROR_PURCHASE_AMOUNT_POSITIVE(() -> "구입 금액은 양수여야 합니다."),
-    ERROR_PURCHASE_AMOUNT_ZERO(() -> "구입 금액은 0원일 수 없습니다."),
-    ERROR_PURCHASE_AMOUNT_MAX(() -> "최대 구입 금액을 초과했습니다."),
-    ERROR_LOTTO_NUMBER(() -> String.format("로또 번호는 %d부터 %d 사이의 정수여야 합니다.", LottoNumberValidation.MIN_NUMBER, LottoNumberValidation.MAX_NUMBER)),
-    ERROR_LOTTO_NUMBER_COUNT(() -> "로또 번호는 6개여야 합니다."),
-    ERROR_DUPLICATE_LOTTO_NUMBER(() -> "로또 번호는 중복될 수 없습니다."),
-    ERROR_WINNING_NUMBER_FORMAT(() -> "당첨 번호는 쉼표(,)로 구분된 숫자여야 합니다."),
-    ERROR_BONUS_NUMBER(() -> "보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    PURCHASE_AMOUNT_UNIT(() -> "구입 금액은 1,000원 단위로 입력해야 합니다."),
+    PURCHASE_AMOUNT_POSITIVE(() -> "구입 금액은 양수여야 합니다."),
+    PURCHASE_AMOUNT_ZERO(() -> "구입 금액은 0원일 수 없습니다."),
+    PURCHASE_AMOUNT_MAX(() -> "최대 구입 금액을 초과했습니다."),
+    WINNING_NUMBER_RANGE(() -> String.format("당첨 번호는 %d부터 %d 사이의 정수여야 합니다.", LottoNumberValidation.MIN_NUMBER, LottoNumberValidation.MAX_NUMBER)),
+    WINNING_NUMBER_COUNT(() -> "당첨 번호는 6개여야 합니다."),
+    DUPLICATE_WINNING_NUMBER(() -> "당첨 번호는 중복될 수 없습니다."),
+    WINNING_NUMBER_FORMAT(() -> "당첨 번호는 쉼표(,)로 구분된 숫자여야 합니다."),
+    DUPLICATE_BONUS_NUMBER(() -> "보너스 번호는 당첨 번호와 중복될 수 없습니다.");
 
     private static final String ERROR_MARK = "[ERROR]";
     private final Supplier<String> messageSupplier;
