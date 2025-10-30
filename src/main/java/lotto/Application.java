f381433ed7de2d6ba4c97bb9f40b68001e924c95
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-30 14:59:01 +0000

    feat(Apllication):프로그램 구동을 위한 코드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.Lotto;
+import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
+import lotto.model.LottoMakeService;
+import lotto.model.LotteryCheckService;
+import lotto.model.LottoProfitService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        LottoInputView lottoInputView = new LottoInputView();
+        LottoOutputView lottoOutputView = new LottoOutputView();
+        LottoMakeService lottoMakeService = new LottoMakeService();
+        LotteryCheckService lotteryCheckService = new LotteryCheckService();
+        LottoProfitService lottoProfitService = new LottoProfitService();
+        LottoController lottoController = new LottoController(lottoInputView, lottoOutputView, lottoMakeService,
+                lotteryCheckService, lottoProfitService);
+
+        lottoController.run();
     }
 }
