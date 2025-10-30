78c74d4419e8d86923d1ff7776e8aa2409d59d3e
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-30 11:49:03 +0000

    refactor(LotteryCheckService):서비스를 stateless 하게 변환

--- src/main/java/lotto/model/LotteryCheckService.java
+++ src/main/java/lotto/model/LotteryCheckService.java
@@ -3,45 +3,40 @@ package lotto.model;
 import lotto.config.WinningCondition;
 
 public class LotteryCheckService {
-    
-    private final LottoRepository lottoRepository;
-    private final WinningRepository winningRepository;
-    private final WinningNumber winningNumber;
-    private final BonusNumber bonusNumber;
 
-    public LotteryCheckService(LottoRepository lottoRepository, WinningRepository winningRepository, 
-    WinningNumber winningNumber, BonusNumber bonusNumber) {
-        this.lottoRepository = lottoRepository;
-        this.winningRepository = winningRepository;
-        this.winningNumber = winningNumber;
-        this.bonusNumber = bonusNumber;
-    }
-
-    public void lotteryCheck() {
-        for(Lotto lotto : lottoRepository.getLottos()) {
-            checkConditions(lotto);
+    public void lotteryCheck(LottoRepository lottoRepository,
+            WinningRepository winningRepository,
+            WinningNumber winningNumber,
+            BonusNumber bonusNumber) {
+        for (Lotto lotto : lottoRepository.getLottos()) {
+            checkConditions(lotto, winningRepository, winningNumber, bonusNumber);
         }
     }
 
-    public void checkConditions(Lotto lotto) {
-        for(WinningCondition winningCondition : WinningCondition.values()) {
-            if(checkMatchCondition(lotto, winningCondition)) {
+    private void checkConditions(Lotto lotto,
+            WinningRepository winningRepository,
+            WinningNumber winningNumber,
+            BonusNumber bonusNumber) {
+        for (WinningCondition winningCondition : WinningCondition.values()) {
+            if (checkMatchCondition(lotto, winningCondition, winningNumber, bonusNumber)) {
                 winningRepository.addCount(winningCondition);
                 return;
             }
         }
     }
 
-    public boolean checkMatchCondition(Lotto lotto, WinningCondition winningCondition) {
+    private boolean checkMatchCondition(Lotto lotto,
+            WinningCondition winningCondition,
+            WinningNumber winningNumber,
+            BonusNumber bonusNumber) {
         int matchCount = (int) lotto.getNumbers().stream()
-            .filter(number -> winningNumber.getWinningNumbers().contains(number))
-            .count();
+                .filter(number -> winningNumber.getWinningNumbers().contains(number))
+                .count();
 
         if (winningCondition == WinningCondition.SECOND) {
-            return matchCount == winningCondition.getMatchCount() &&
-                lotto.getNumbers().contains(bonusNumber.getValue());
+            return matchCount == winningCondition.getMatchCount()
+                    && lotto.getNumbers().contains(bonusNumber.getValue());
         }
-
         return matchCount == winningCondition.getMatchCount();
     }
 }
--- src/test/java/lotto/model/LotteryCheckServiceTest.java
+++ src/test/java/lotto/model/LotteryCheckServiceTest.java
@@ -15,26 +15,24 @@ public class LotteryCheckServiceTest {
     void testLotteryCheckService() {
 
         LottoRepository lottoRepository = new LottoRepository(List.of(
-            new Lotto(List.of(1,2,3,4,5,6)), //1등
-            new Lotto(List.of(1,2,3,4,5,7)), //2등
-            new Lotto(List.of(1,2,3,4,5,8)), //3등
-            new Lotto(List.of(1,2,3,4,7,8)), //4등
-            new Lotto(List.of(1,2,3,7,8,9)), //5등
-            new Lotto(List.of(10,11,12,13,14,15)) //낙첨
-        )); 
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)), // 1등
+                new Lotto(List.of(1, 2, 3, 4, 5, 7)), // 2등
+                new Lotto(List.of(1, 2, 3, 4, 5, 8)), // 3등
+                new Lotto(List.of(1, 2, 3, 4, 7, 8)), // 4등
+                new Lotto(List.of(1, 2, 3, 7, 8, 9)), // 5등
+                new Lotto(List.of(10, 11, 12, 13, 14, 15)) // 낙첨
+        ));
         WinningRepository winningRepository = new WinningRepository();
         WinningNumber winningNumber = new WinningNumber(List.of("1", "2", "3", "4", "5", "6"));
         BonusNumber bonusNumber = new BonusNumber("7", winningNumber);
 
-        LotteryCheckService lotteryCheckService = new LotteryCheckService(
-            lottoRepository, winningRepository, winningNumber, bonusNumber
-        );
+        LotteryCheckService lotteryCheckService = new LotteryCheckService();
 
-        lotteryCheckService.lotteryCheck();
+        lotteryCheckService.lotteryCheck(lottoRepository, winningRepository, winningNumber, bonusNumber);
 
-        for(WinningCondition winningCondition : WinningCondition.values()) {
+        for (WinningCondition winningCondition : WinningCondition.values()) {
             assertEquals(1L, winningRepository.getAllCounts().get(winningCondition));
         }
     }
-    
+
 }
