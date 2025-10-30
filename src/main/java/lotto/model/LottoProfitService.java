8a787570e43670863c8813916eb0ea7ae784dbab
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-30 14:08:16 +0000

    feat(LottoProfitService):로또 수익률 계산 기능 추가

---
+++ src/main/java/lotto/model/LottoProfitService.java
@@ -0,0 +1,14 @@
+package lotto.model;
+
+public class LottoProfitService {
+
+    public double calculateProfitRate(PurchaseAmount purchaseAmount, WinningRepository winningRepository) {
+        Long totalPrize = winningRepository.getTotalPrize();
+        Long totalCost = purchaseAmount.getPurchaseAmount();
+        if (totalCost == 0) {
+            return 0.0;
+        }
+        return (double) totalPrize / totalCost * 100;
+    }
+    
+}
