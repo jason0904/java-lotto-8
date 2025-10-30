a2f6b07dbf48c7cc4c4ed617eff6de7a023d8121
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-30 09:33:40 +0000

    feat(WinningRepository):당첨기록을 저장해놓고 불러오는 기능 구현.

---
+++ src/main/java/lotto/model/WinningRepository.java
@@ -0,0 +1,30 @@
+package lotto.model;
+
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Objects;
+
+import lotto.config.WinningCondition;
+
+public class WinningRepository {
+
+    private final EnumMap<WinningCondition, Long> winningCounts = new EnumMap<>(WinningCondition.class);
+
+    public WinningRepository() {
+        for(WinningCondition winningCondition : WinningCondition.values()) {
+            winningCounts.put(winningCondition, 0L);
+        }
+    }
+
+    public void addCount(WinningCondition winningCondition) {
+        Objects.requireNonNull(winningCondition);
+        winningCounts.put(winningCondition, winningCounts.getOrDefault(winningCondition, 0L) + 1L);
+    }
+
+    public Map<WinningCondition, Long> getAllCounts() {
+        return Collections.unmodifiableMap(winningCounts);
+    }
+
+    
+}
