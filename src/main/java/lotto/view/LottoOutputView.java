3839a9fb0a11fa7f55443831619613cc09ca3156
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-30 15:02:36 +0000

    feat(LottoOutputView): 로또발매기의 정해진 출력형식을 맞추기 위한 빈줄 생성 기능 추가

--- src/main/java/lotto/view/LottoOutputView.java
+++ src/main/java/lotto/view/LottoOutputView.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 public class LottoOutputView {
 
     public void printLottos(LottoRepository lottoRepository, PurchaseAmount purchaseAmount) {
-        System.out.printf("%d개를 구매하였습니다.\n", purchaseAmount.getPurchaseAmount() / 1000 );
+        System.out.printf("\n%d개를 구매하였습니다.\n", purchaseAmount.getPurchaseAmount() / 1000 );
         for (Lotto lotto : lottoRepository.getLottos()) {
             String numbers = lotto.getNumbers().stream()
                     .map(String::valueOf)
@@ -35,6 +35,10 @@ public class LottoOutputView {
 
     public void printProfitRate(double profitRate) {
         System.out.printf("총 수익률은 %.2f%%입니다.\n", profitRate);
+    }
+
+    public void printNewLine() {
+        System.out.println();
     }
 
     private String getBonusString(WinningCondition winningCondition) {
