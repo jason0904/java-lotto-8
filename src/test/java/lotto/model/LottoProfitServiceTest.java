a4a10e2b102a0497cd4df52f4b336668e1718f5e
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-30 14:56:36 +0000

    test(LottoProfitService):로또 수익률 계산 테스트 추가

---
+++ src/test/java/lotto/model/LottoProfitServiceTest.java
@@ -0,0 +1,27 @@
+package lotto.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import lotto.config.WinningCondition;
+
+public class LottoProfitServiceTest {
+
+    @Test
+    @DisplayName("로또 수익률 계산 테스트")
+    void testCalculateProfitRate() {
+        PurchaseAmount purchaseAmount = new PurchaseAmount("10000");
+        WinningRepository winningRepository = new WinningRepository();
+        winningRepository.addCount(WinningCondition.FIRST);
+        winningRepository.addCount(WinningCondition.THIRD);
+        LottoProfitService lottoProfitService = new LottoProfitService();
+        double profitRate = lottoProfitService.calculateProfitRate(purchaseAmount, winningRepository);
+        double expectedProfitRate = ((WinningCondition.FIRST.getPrize() + WinningCondition.THIRD.getPrize())
+                / purchaseAmount.getPurchaseAmount()) * 100;
+
+        assertEquals(expectedProfitRate, profitRate);
+    }
+
+}
