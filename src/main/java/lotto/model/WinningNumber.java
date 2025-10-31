d6b3c8a8370f3d51cdb25a703ace66af141f553c
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-31 13:50:43 +0000

    refactor(WinningNumber):코드 스타일 적용, 중복 파싱 방지를 위한 숫자 검증 및  파싱 로직 통합

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -1,8 +1,9 @@
 package lotto.model;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 import lotto.config.LottoRule;
 import lotto.validation.ErrorMessage;
@@ -12,56 +13,50 @@ public class WinningNumber {
     private final List<Integer> winningNumbers;
 
     public WinningNumber(List<String> input) {
-        validate(input);
-        this.winningNumbers = input.stream()
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-    }
-    
-    private void validate(List<String> input) {
-        validateLottoSize(input.size());
-
-        for(String number : input) {
-            validateNumberIsInteger(number);
-            validateNumberRange(Integer.parseInt(number));
-        }
-
-        validateDuplicateNumber(input);
-    }
-
-    private void validateLottoSize(int size) {
-        if (size != LottoRule.SIZE.getValue()) {
-            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_SIZE.getMessage());
-        }
-    }
-
-    private void validateNumberIsInteger(String numberText) {
-        try {
-            Integer.parseInt(numberText);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_FORMAT.getMessage());
-        }
-    }
-
-    private void validateNumberRange(int number) {
-        if (number < LottoRule.MIN_NUMBER.getValue() || number > LottoRule.MAX_NUMBER.getValue()) {
-            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_RANGE.getMessage());
-        }
-    }
-
-    private void validateDuplicateNumber(List<String> input) {
-        long distinctCount = input.stream()
-                .distinct()
-                .count();
-
-        if (distinctCount != input.size()) {
-            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_WINNING_NUMBER.getMessage());
-        }
+        List<Integer> numbers = validateNumbersAndParse(input);
+        validate(numbers);
+        this.winningNumbers = numbers;
     }
 
     public List<Integer> getWinningNumbers() {
         return Collections.unmodifiableList(winningNumbers);
     }
 
+    private List<Integer> validateNumbersAndParse(List<String> input) {
+        try {
+            return input.stream()
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_FORMAT.getMessage());
+        }
+    }
 
+    private void validate(List<Integer> numbers) {
+        validateLottoSize(numbers);
+        validateNumberRange(numbers);
+        validateDuplicateNumber(numbers);
+    }
+
+    private void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != LottoRule.SIZE.getValue()) {
+            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_SIZE.getMessage());
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < LottoRule.MIN_NUMBER.getValue() || number > LottoRule.MAX_NUMBER.getValue()) {
+                throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_RANGE.getMessage());
+            }
+        }
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> distinctNumbers = new HashSet<>(numbers);
+        if (distinctNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_WINNING_NUMBER.getMessage());
+        }
+    }
+    
 }
