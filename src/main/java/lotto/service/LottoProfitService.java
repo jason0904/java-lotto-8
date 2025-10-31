5325396e679e0a9dcfcfe44f4eb727c9fb704f24
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-31 14:59:06 +0000

    refactor(LottoProfitService): 수익률 계산 책임을 WinningRepository로 옮김

--- src/main/java/lotto/service/LottoProfitService.java
+++ src/main/java/lotto/service/LottoProfitService.java
@@ -6,12 +6,7 @@ import lotto.model.WinningRepository;
 public class LottoProfitService {
 
     public double calculateProfitRate(PurchaseAmount purchaseAmount, WinningRepository winningRepository) {
-        Long totalPrize = winningRepository.getTotalPrize();
-        Long totalCost = purchaseAmount.getPurchaseAmount();
-        if (totalCost == 0) {
-            return 0.0;
-        }
-        return (double) totalPrize / totalCost * 100;
+        return winningRepository.calculateTotalProfitRate(purchaseAmount);
     }
     
 }
