30379081ba93aff370b2cd7f618abc5262170444
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-31 14:36:17 +0000

    test(WinningCondition): 당첨 여부 판단 테스트 추가

---
+++ src/test/java/lotto/model/WinningConditionTest.java
@@ -0,0 +1,34 @@
+package lotto.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import lotto.config.WinningCondition;
+
+public class WinningConditionTest {
+    
+    @Test
+    @DisplayName("당첨 기록 확인 테스트")
+    void testLotteryCheckService() {
+        List<Lotto> lottos = List.of(
+            new Lotto(List.of(1, 2, 3, 4, 5, 6)), // 1등
+            new Lotto(List.of(1, 2, 3, 4, 5, 7)), // 2등
+            new Lotto(List.of(1, 2, 3, 4, 5, 8)), // 3등
+            new Lotto(List.of(1, 2, 3, 4, 7, 8)), // 4등
+            new Lotto(List.of(1, 2, 3, 7, 8, 9)), // 5등
+            new Lotto(List.of(10, 11, 12, 13, 14, 15)) // 낙첨
+        );
+
+        WinningNumber winningNumber = new WinningNumber(List.of("1", "2", "3", "4", "5", "6"));
+        BonusNumber bonusNumber = new BonusNumber("7", winningNumber);
+
+        for(int i = 0; i < lottos.size(); i++) {
+            WinningCondition winningCondition = WinningCondition.fromMatchCounts(lottos.get(i), winningNumber, bonusNumber);
+            assertEquals(WinningCondition.values()[i], winningCondition);
+        }
+    }
+
+}
