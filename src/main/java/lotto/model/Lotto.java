3633056b34004a55bd587a8365548c12be3c6d93
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-31 10:13:29 +0000

    refactor(Lotto):로또 숫자 저장형식 불변 리스트로 변경. 로또 숫자 유효성 검사 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -8,27 +8,43 @@ import lotto.config.LottoRule;
 import lotto.validation.ErrorMessage;
 
 public class Lotto {
-    
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = new ArrayList<>(numbers);
-        sortNumbers();
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != LottoRule.SIZE.getValue()) {
-            throw new IllegalArgumentException(ErrorMessage.LOTTO_SIZE.getMessage());
-        }
-    }
-
-    private void sortNumbers() {
-        numbers.sort(Integer::compareTo);
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        this.numbers = Collections.unmodifiableList(sortedNumbers);
     }
 
     public List<Integer> getNumbers() {
         return Collections.unmodifiableList(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        numbers.forEach(this::validateNumberRange);
+        validateDuplicateNumbers(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LottoRule.SIZE.getValue()) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_SIZE.getMessage());
+        }
+    }
+
+    private void validateNumberRange(int value) {
+        if (value < LottoRule.MIN_NUMBER.getValue() || value > LottoRule.MAX_NUMBER.getValue()) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_RANGE.getMessage());
+        }
+    }
+
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        long distinctCount = numbers.stream().distinct().count();
+        if (distinctCount != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBER.getMessage());
+        }
     }
 
 }
