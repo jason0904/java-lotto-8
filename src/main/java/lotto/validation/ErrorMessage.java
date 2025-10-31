accc70e3770131f697736295a1a06be2684cc432
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-31 10:08:51 +0000

    refactor(ErrorMessage):에러메시지 세분화 및 순서 변경

--- src/main/java/lotto/validation/ErrorMessage.java
+++ src/main/java/lotto/validation/ErrorMessage.java
@@ -11,9 +11,11 @@ public enum ErrorMessage {
     PURCHASE_AMOUNT_NOT_LONG(() -> "최대 구입 금액이 정수가 아닙니다."),
     WINNING_NUMBER_RANGE(() -> String.format("당첨 번호는 %d부터 %d 사이의 정수여야 합니다.", LottoRule.MIN_NUMBER.getValue(), LottoRule.MAX_NUMBER.getValue())),
     WINNING_NUMBER_SIZE(() -> "당첨 번호는 6개여야 합니다."),
-    LOTTO_SIZE(() -> "로또 번호는 6개여야 합니다."),
     DUPLICATE_WINNING_NUMBER(() -> "당첨 번호는 중복될 수 없습니다."),
     WINNING_NUMBER_FORMAT(() -> "당첨 번호는 쉼표(,)로 구분된 숫자여야 합니다."),
+    LOTTO_NUMBER_RANGE(() -> String.format("로또 번호는 %d부터 %d 사이의 정수여야 합니다.", LottoRule.MIN_NUMBER.getValue(), LottoRule.MAX_NUMBER.getValue())),
+    LOTTO_NUMBER_SIZE(() -> "로또 번호는 6개여야 합니다."),
+    DUPLICATE_LOTTO_NUMBER(() -> "로또 번호는 중복될 수 없습니다."),
     DUPLICATE_BONUS_NUMBER(() -> "보너스 번호는 당첨 번호와 중복될 수 없습니다."),
     STRING_STARTS_WITH_COMMA(() -> "입력 값은 쉼표(,)로 시작할 수 없습니다."),
     STRING_ENDS_WITH_COMMA(() -> "입력 값은 쉼표(,)로 끝날 수 없습니다.");
