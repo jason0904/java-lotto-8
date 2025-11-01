8157f26687eaad6149d548f5a38fe48b1bcc4aa9
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-11-01 04:04:58 +0000

    refactor(LottoInputView):MVC패턴에 따라 입력을 Controller의 책임으로 위임, 이 클래스는 입력시의 문구만 띄우는 것으로 변경

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -1,39 +1,22 @@
 package lotto.view;
 
-import camp.nextstep.edu.missionutils.Console;
-import lotto.model.BonusNumber;
-import lotto.model.PurchaseAmount;
-import lotto.model.WinningNumber;
-import lotto.validation.SplitStringValidation;
-
-import java.util.List;
-
 public class LottoInputView {
 
     private final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해 주세요.";
     private final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public PurchaseAmount purchaseInput() {
+    public void showPurchaseInputMessage() {
         System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
-        return new PurchaseAmount(Console.readLine());
+        
     }
 
-    public WinningNumber winningNumberInput() {
+    public void showWinningNumberInputMessage() {
         System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
-        String input = Console.readLine();
-        SplitStringValidation.validateComma(input);
-        List<String> numbers = java.util.Arrays.asList(input.split(","));
-        return new WinningNumber(numbers);
     }
 
-    public BonusNumber bonusNumberInput(WinningNumber winningNumber) {
+    public void showBonusNumberInputMessage() {
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
-        String input = Console.readLine();
-        return new BonusNumber(input, winningNumber);
     }
-
-
-
 
 }
