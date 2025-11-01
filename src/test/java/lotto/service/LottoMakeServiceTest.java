fc6c3f03c9753d9da5c4290b75bd68a7f4c2722d
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-11-01 05:26:41 +0000

    test(LottoMakeService):로또 한장 만드는 서비스 테스트는 여러장 생성 서비스에서도 검증이 되므로 제거

--- src/test/java/lotto/service/LottoMakeServiceTest.java
+++ src/test/java/lotto/service/LottoMakeServiceTest.java
@@ -15,21 +15,6 @@ import org.junit.jupiter.api.DisplayName;
 public class LottoMakeServiceTest {
     
     @Test
-    @DisplayName("로또 생성 서비스 테스트")
-    void testLottoMakeService() {
-        LottoMakeService lottoMakeService = new LottoMakeService();
-        Lotto lotto = lottoMakeService.makeLotto();
-        
-        assertEquals(lotto.getNumbers().size(), 6);
-
-        assertThat(lotto.getNumbers().stream()
-        .allMatch(n -> n >= LottoRule.MIN_NUMBER.getValue() && n <= LottoRule.MAX_NUMBER.getValue()))
-        .isTrue();
-
-        assertThat(lotto.getNumbers()).isSorted();
-    }
-
-    @Test
     @DisplayName("로또 여러장 생성 서비스 테스트")
     void testLottoMakeMultipleService() {
         LottoMakeService lottoMakeService = new LottoMakeService();
